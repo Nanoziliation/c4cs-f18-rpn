58478b8639ffd7180741b418fc40dd47dcc1b800
egajda <egajda@umich.edu>
2018-10-26 15:00:50 -0400

    Implement add

--- rpn.py
+++ rpn.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
+    stack = []
+    tokens = arg.split()
+    for token in tokens:
+        try:
+            stack.append(int(token))
+        except ValueError:
+            val1 = stack.pop()
+            val2 = stack.pop()
+            result = val1 + val2
+
+            stack.append(result)
+    return stack[0]
+
     pass
 
 def main():
     while True:
-        calculate(input("rpn clac> "))
+        result = calculate(input("rpn clac> "))
+        print(result)
 
 if __name__ == '__main__':
     main()
